3e2416adad1764ec0e388b833ea379b25032dc11
sayr0s <81544330+sayr0s@users.noreply.github.com>
2025-05-11 23:30:24 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   //API:"https://koorea1.onrender.com"
   // API:"http://api.eurobet.shop"
-  API:"http://51.222.111.183:8080"
+  API:"https://backendcasino-oqen.onrender.com"
 };
